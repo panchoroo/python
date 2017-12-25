6a40cb18492fa5b3418f8159e3826339ab136c03
Amie Everett <amie.everett@gmail.com>
2017-12-25 15:11:53 -0500

    changed error functions to return number of errors and added tabu simulated annealing to mutate possible solutions

--- prob96.py
+++ prob96.py
@@ -9,6 +9,8 @@
 # By solving all fifty puzzles find the sum of the 3-digit numbers found in the top left corner of each solution 
 # grid; for example, 483 is the 3-digit number found in the top left corner of the solution grid above.
 
+import random;
+
 print 'derp'
 sudokuText= "p096_sudoku.txt"
 def loadSuccesses():
@@ -68,6 +70,7 @@ for p in puzzleList:
 	# '856789123', 
 	# '989123456'];
 
+	# test solution 
 	# possiblySolved=['123456789', 
 	# '456789123', 
 	# '789123456', 
@@ -78,73 +81,139 @@ for p in puzzleList:
 	# '678912345', 
 	# '912345678'];
 
-	def gridChecker(rowStart, colStart):
-		gridNums = '';
-		count = 0;
-		line = possiblySolved[rowStart];
-		colCount = 0;
+	def checkAllErrors(possibleSolution):
 
-		while count < 9:
-			# print line, count;
-			if count == 3:
-				line = possiblySolved[rowStart+1];
-				colCount = 0;
-			elif count == 6: 
-				line = possiblySolved[rowStart+2];
-				colCount = 0;
+		def gridChecker(rowStart, colStart):
+			gridNums = '';
+			count = 0;
+			line = possibleSolution[rowStart];
+			colCount = 0;
+			errors = 0;
 
-			digit = line[colStart+colCount];
-			if digit in gridNums:
-				# print digit, gridNums;
-				return False;
-			else:
-				gridNums += digit;
-			count += 1;
-			colCount += 1;
-		# print gridNums;
-		return True;
+			while count < 9:
+				# print line, count;
+				if count == 3:
+					line = possibleSolution[rowStart+1];
+					colCount = 0;
+				elif count == 6: 
+					line = possibleSolution[rowStart+2];
+					colCount = 0;
 
-	gridRowIndex = 0;
+				digit = line[colStart+colCount];
+				if digit in gridNums:
+					# print digit, gridNums;
+					errors += 1;
+				else:
+					gridNums += digit;
+				count += 1;
+				colCount += 1;
+			# print gridNums;
+			return errors;
 
-	foundErrorGrid = False
-	while gridRowIndex < 7 and not foundErrorGrid:
-		# check each 3x3 grid for numbers 1-9, no repeats
-		gridColIndex = 0;
-		while gridColIndex < 7:
-			if not gridChecker (gridRowIndex, gridColIndex):
-				foundErrorGrid = True;
-			gridColIndex += 3;
-		gridRowIndex += 3;
-	# print 'found error in grid'
+		def colChecker():
+			errors = 0;
+			colIndex = 0;
+			rowIndex = 0
+			while colIndex < 9:
+				colNums = '';
+				while rowIndex < 9:
+					# print possibleSolution[rowIndex][colIndex];
+					if possibleSolution[rowIndex][colIndex] not in colNums:
+						colNums += possibleSolution[rowIndex][colIndex];
+					else: 
+						errors += 1;
+						# print 'nope'
+					
+					rowIndex += 1;
+				colIndex += 1;
+				rowIndex = 0;
+			return errors;
 
-	# if no errors found in grids, check columns
-	foundErrorGrid = False;
-	if not foundErrorGrid:
-		foundErrorCol = False;
-		# print 'no errors'
-		colIndex = 0;
-		rowIndex = 0
-		while colIndex < 9 and not foundErrorCol:
-			colNums = '';
-			while rowIndex < 9 and not foundErrorCol:
-				print possiblySolved[rowIndex][colIndex];
-				if possiblySolved[rowIndex][colIndex] not in colNums:
-					colNums += possiblySolved[rowIndex][colIndex];
-				else: 
-					foundErrorCol = True;
-					# print 'nope'
-					break;
-				
-				rowIndex += 1;
-			colIndex += 1;
-			rowIndex = 0;
 
-	# foundErrorGrid = True;
-	if not foundErrorGrid and not foundErrorCol:
+		gridRowIndex = 0;
+		allErrors = 0;
+		while gridRowIndex < 7:
+			# check each 3x3 grid for numbers 1-9, no repeats
+			gridColIndex = 0;
+			while gridColIndex < 7:
+				allErrors = gridChecker(gridRowIndex, gridColIndex);
+				gridColIndex += 3;
+			gridRowIndex += 3;
+
+		allErrors += colChecker();
+		return allErrors;
+
+	# while totalErrors > 0: 
+	totalErrors = checkAllErrors(possiblySolved);
+	if totalErrors == 0:
 		# add to actual solutions
 		print 'yay!'
 	else: 
 		print 'nerrrrp'
+		print totalErrors;
+
+	def mutationMaker(originalPuzzle, currentVersion):
+		# could make it have greater mutations based on number of errors?
+		# make mutations smaller as it gets closer
+		lineCount = 0;
+		switchables = [];
+		mutatedPuzzle = [];
+		while lineCount < 9:
+			mutatedLine = '';
+			for digit in currentVersion[lineCount]:
+				if digit not in originalPuzzle[lineCount]:
+					switchables.append(digit);
+			if len(switchables) > 1:
+				# maybe dont need this?
+				for digit in currentVersion[lineCount]:
+					if digit in originalPuzzle[lineCount]:
+						mutatedLine += digit;
+					else:
+						randomIndex = random.randint(0,len(switchables)-1)
+						mutatedLine += switchables[randomIndex];
+						switchables.remove(switchables[randomIndex]);
+						# switchables = switchables[:switchables.index(randomIndex)] + switchables[switchables.index(randomIndex + 1):]
+				mutatedPuzzle.append(mutatedLine);
+			lineCount += 1;
+		# print 'returns?'
+		return mutatedPuzzle;
+
+	# print mutationMaker(workingPuzzle, possiblySolved);
+
+	# simulated annealing --> tabu: generate multiple versions with mutations and keep one with fewest errors
+	
+	def tabuMaker(possiblySolved):
+		tabus = [];
+		tabuCount = 10;
+		while tabuCount > 0:
+			tabus.append(mutationMaker(workingPuzzle, possiblySolved));
+			tabuCount -= 1;
+
+		tabuErrors = [];
+		for t in tabus:
+			tabuErrors.append(checkAllErrors(t));
+
+		print sorted(tabuErrors)[0];
+		if sorted(tabuErrors)[0] == 0:
+			print '0 errors!'
+		# only accept mutations that DECREASE number of errors
+		# print tabuErrors;
+		# check if errors == 0?
+		bestMutation = tabus[tabuErrors.index(sorted(tabuErrors)[0])]; 
+		# print bestMutation;
+		return bestMutation
+
+	# mutationMaker()
+
+	mutations = 0;
+	newBest = tabuMaker(possiblySolved);
+
+	while mutations < 100:
+		newBest = tabuMaker(newBest);
+		print newBest;
+		mutations += 1;
+
+	# keep checking for errors until equals 0
 
 	# once error has been found, shuffle the numbers
 	
@@ -153,127 +222,4 @@ for p in puzzleList:
 
 
 
-
-
-
-	# for solvedLine in possiblySolved:
-		# for solvedDigit in solvedLine:
-			# check each digit of each line to see if this possible solution has any errors
-			# check columns for > 1 of same digit, (rows should already be error-free byt design)
-
-# 	def getGrid(index):
-# 		firstGrid = [0,1,2];
-# 		secondGrid = [3,4,5];
-# 		thirdGrid = [6,7,8];
-# 		if index in firstGrid:
-# 			return firstGrid;
-# 		elif index in secondGrid:
-# 			return secondGrid;
-# 		elif index in thirdGrid:
-# 			return thirdGrid;
-# 		else:
-# 			print('something has gone terribly, terribly wrong');
-
-# 	currentGrid = [0,1,2];
-# 	checkIndex = 0;
-
-# 	def rowDigitChecker(digit):
-# 		noNew = True;
-# 		def currentCheck(line):	
-# 			return line.index(digit);
-# 			# checkIndex = line.index(digit);
-# 			# if workingPuzzle.index(line) not in currentGrid:
-# 			# 	newGrid = getGrid(workingPuzzle.index(line));
-# 			# return newGrid;
-
-# 		def currentGridChecker(line, grid):
-			
-# 			newGrid = grid;
-# 			# checkIndex = line.index(digit);
-# 			if workingPuzzle.index(line) not in grid:
-# 				newGrid = getGrid(workingPuzzle.index(line));
-# 			return newGrid;
-
-# 		line = 0;
-# 		currentGrid = getGrid(line);
-
-# 		lineIndex = 0;
-# 		while lineIndex < 7:
-# 			workingLine = workingPuzzle[lineIndex]
-# 			first = [digit in workingLine, 0];
-# 			second = [digit in workingPuzzle[workingPuzzle.index(workingLine)+1], 1];
-# 			third = [digit in workingPuzzle[workingPuzzle.index(workingLine)+2], 2];
-# 			found = [first, second, third];
-# 			successes = [f[1] for f in found if f[0]]
-# 			# print ('successes', successes)
-
-# 			if len(successes) == 2:
-# 				print 'heyo'
-# 				# figure out which two column grids have been done already, 
-# 				# check that rows' grids' 0's
-# 				# Get one that's not used and check other rows at 0's indices 
-# 				line1 = workingPuzzle[workingPuzzle.index(workingLine)+successes[0]];
-# 				index1 = line1.index(digit);
-# 				grid1 = getGrid(index1);
-
-# 				line2 = workingPuzzle[workingPuzzle.index(workingLine)+successes[1]];
-# 				index2 = line1.index(digit);
-# 				grid2 = getGrid(index2);
-
-# 				if (0 in grid1 or 0 in grid2 and (3 in grid1 or 3 in grid2)):
-# 					grid3 = [6,7,8];
-# 				elif (0 in grid1 or 0 in grid2 and (6 in grid1 or 6 in grid2)):
-# 					grid3 = [3,4,5];
-# 				else:
-# 					grid3 = [0,1,2];
-
-# 				print ('lines',workingPuzzle.index(line1), workingPuzzle.index(line2))
-# 				if (successes[1] - successes[0] == 2):
-# 					line3 = workingPuzzle[workingPuzzle.index(workingLine)+1];
-# 				elif (workingPuzzle.index(line1) == 0 or workingPuzzle.index(line1) == 3 or workingPuzzle.index(line1) == 6):
-# 					line3 = workingPuzzle[workingPuzzle.index(workingLine)+2];
-# 				else:
-# 					line3 = workingPuzzle[workingPuzzle.index(workingLine)];
-				
-# 				# searching line 3 w/i grid 3 for 0's to find available
-# 				# comparing column at indicies of 0s to see if we can rule any out
-# 				print ('3s ', line3, grid3);
-# 				zeroes = []
-# 				for g in grid3:
-# 					print line3[g]
-# 					# maybe filter grid3 somehow?
-# 					if (str(line3[g]) == '0'):
-# 						zeroes.append(g);
-						
-# 				matches = [];
-# 				for z in zeroes:
-# 					# print ('z? ',z)
-# 					for testLine in workingPuzzle:
-# 						if testLine[z] == digit:
-# 							zeroes.remove(z);
-# 				if len(zeroes) == 1:
-# 					print 'YEAY'
-# 					# print (line3, digit, zeroes[0])
-# 					noNew = False;
-
-# 					newLine = line3[:zeroes[0]] + str(digit) + line3[zeroes[0]+1:];
-# 					# newLine[zeroes[0]] = str(digit);
-# 					workingPuzzle[workingPuzzle.index(line3)] = newLine;
-
-# 			lineIndex += 3;
-# 		return noNew;
-
-
-# 	done = False;
-# 	while not done:
-# 		allDone = True;
-# 	 	for digit in possibleDigits:
-# 	 		if rowDigitChecker(digit) == False:
-# 	 			allDone = False;
-# 	 	if allDone == True:
-# 	 		done = True;
-
-# 	# rowDigitChecker(possibleDigits[0]);
-
-# print workingPuzzle;
 
